4c1430e4ad49f2e09840c4161170e859ff1be54e
kinnesotan <kinmatw@dunwoody.edu>
2016-01-27 12:58:31 -0600

    Started the site
    Basic outline of nav-bar and logo

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Code Plateau</title>
+    <link rel="stylesheet" media="(min-width: 1000px)" href="css/desktopstyles.css" />
+</head>
+
+<body>
+    <div class="desktop-view">
+        <div id="top-bar">
+            <div id="logo"><img src="images/CodePlateau.png" width="150" height="38"></div>
+            <div id="desktop-nav">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Code</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        </div>
+        
+    </div>
+    <div class="mobile-view">
+        
+    </div>
+
+
+
+</body>
+</html>
