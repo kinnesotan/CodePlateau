ba479a8a2ed7511a9a8cdbc512307bddb1c47bf0
kinnesotan <kinmatw@dunwoody.edu>
2016-01-28 11:48:09 -0600

    Added mobile view, css, and images
    Tweaked a few things, edited the CSS, and now the logo is clickable to
    take you back to the homepage.

--- index.php
+++ index.php
@@ -4,26 +4,70 @@
 <head>
     <title>Code Plateau</title>
     <link rel="stylesheet" media="(min-width: 1000px)" href="css/desktopstyles.css" />
+    <link rel="stylesheet" media="(max-width: 999px)" href="css/mobileview.css" />
+    <script src="js/modernizr.custom.js"></script>
+    <link rel="shortcut icon" href="images/favicon.ico">
 </head>
 
-<body>
+<body class="cbp-spmenu-push">
     <div class="desktop-view">
         <div id="top-bar">
-            <div id="logo"><img src="images/CodePlateau.png" width="150" height="38"></div>
+            <div id="logo"><a href="index.php"><img src="images/CodePlateau.png" alt="Code Plateau logo desktop"width="150" height="38"></a></div>
             <div id="desktop-nav">
             <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Code</a></li>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Home</a></li>
+                <li><a href="#">Exercises</a></li>
                 <li><a href="#">About Us</a></li>
             </ul>
         </div>
         </div>
         
     </div>
+<!-- MOBILE VIEW STARTS HERE -->
     <div class="mobile-view">
-        
+        <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+            <h3>Welcome!</h3>
+            <a href="#">Home</a>
+            <a href="#">Code</a>
+            <a href="#">Exercises</a>
+            <a href="#">About Us</a>
+	</nav>
+		<div class="container">
+			<div id="top-bar">
+                            <div id="shownav"><img src="images/menu-icon.png" width="35" height="35"></div>
+                            <div id="logo">
+                                <a href="index.php"><img src="images/CodePlateau.png" alt="Code Plateau logo mobile" width="150" height="38"></a>
+                            </div>
+                        </div>
+			<div id="header"></div>
+			<div class="main">
+				
+			</div>
+			    <div id="footer">
+				<div id="footer-content">
+				</div>
+			    </div>
+		</div>
+    </div>
+		<!-- Classie - class helper functions by @desandro https://github.com/desandro/classie -->
+		<script src="js/classie.js"></script>
+		<script>
+			var menuLeft = document.getElementById( 'cbp-spmenu-s1' ),
+				showLeftPush = document.getElementById( 'shownav' ),	
+				body = document.body;
+
+			showLeftPush.onclick = function() {
+				classie.toggle( this, 'active' );
+				classie.toggle( body, 'cbp-spmenu-push-toright' );
+				classie.toggle( menuLeft, 'cbp-spmenu-open' );
+				disableOther( 'showLeftPush' );
+			};
+		</script>
+		
+		
+        </div>
+<!-- MOBILE CONTENT ENDS HERE -->
     </div>
 
 
